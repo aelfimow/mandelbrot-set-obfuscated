5d40d3c3e670782da9426fa82e21fc8565f5d06e
Alexander Elfimow <my.free.mail@gmx.de>
2018-10-19 22:33:42 +0200

    main: create window

--- src/main.c
+++ src/main.c
@@ -1,11 +1,95 @@
 #include <Windows.h>
+#include <string.h>
+
+#define IDM_APP_EXIT    40000
+
+static const TCHAR MainWindowName[] = TEXT("Mandelbrot Set");
+
+static void MessageBox_Error(const TCHAR *errStr);
+static HMENU MainWindowMenu(void);
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
-    hInstance = hInstance;
     hPrevInstance = hPrevInstance;
     szCmdLine = szCmdLine;
     iCmdShow = iCmdShow;
 
+    WNDCLASS wndclass;
+    memset(&wndclass, 0, sizeof(wndclass));
+
+    wndclass.style         = (CS_HREDRAW | CS_VREDRAW);
+    wndclass.lpfnWndProc   = WndProc;
+    wndclass.cbClsExtra    = 0;
+    wndclass.cbWndExtra    = 0;
+    wndclass.hInstance     = hInstance;
+    wndclass.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszMenuName  = NULL;
+    wndclass.lpszClassName = MainWindowName;
+
+    __auto_type RegisterClassResult = RegisterClass(&wndclass);
+
+    if (0 == RegisterClassResult)
+    {
+        MessageBox_Error(TEXT("Error in RegisterClass"));
+        return 0;
+    }
+
+    __auto_type hWindow = CreateWindow(
+            MainWindowName,
+            MainWindowName,
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            NULL,
+            MainWindowMenu(),
+            hInstance,
+            NULL);
+
+    if (hWindow == NULL)
+    {
+        MessageBox_Error(TEXT("Error in CreateWindow"));
+        return 0;
+    }
+
+    ShowWindow(hWindow, iCmdShow);
+
+    __auto_type UpdateWindowResult = UpdateWindow(hWindow);
+
+    if (UpdateWindowResult == FALSE)
+    {
+        MessageBox_Error(TEXT("Error in UpdateWindow"));
+        return 0;
+    }
+
     return 0;
+}
+
+static void MessageBox_Error(const TCHAR *errStr)
+{
+    (void)MessageBox(NULL, errStr, MainWindowName, MB_ICONERROR);
+}
+
+static HMENU MainWindowMenu(void)
+{
+    __auto_type hCtrlMenu = CreateMenu();
+    {
+        AppendMenu(hCtrlMenu, MF_STRING, IDM_APP_EXIT, TEXT("&Exit"));
+    }
+
+    __auto_type hMenu = CreateMenu();
+    {
+        AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hCtrlMenu, TEXT("&File"));
+    }
+
+    return hMenu;
+}
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
