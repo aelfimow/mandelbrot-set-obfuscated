03b0c66046688b1e6e9e6363aa36dec22417ec3c
Alexander Elfimow <my.free.mail@gmx.de>
2018-10-19 22:19:30 +0200

    Added files

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+    hInstance = hInstance;
+    hPrevInstance = hPrevInstance;
+    szCmdLine = szCmdLine;
+    iCmdShow = iCmdShow;
+
+    return 0;
+}
